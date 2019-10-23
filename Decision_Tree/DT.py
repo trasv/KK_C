727239e108a1f9206d6a82fd1501af4bfed9df2d
Timo <37039325+trasv@users.noreply.github.com>
2019-10-23 13:07:14 +0700

    Update DT.py

--- Decision_Tree/DT.py
+++ Decision_Tree/DT.py
@@ -10,19 +10,6 @@ import math
 from random import randrange
 from random import seed
 
-ys = pd.read_csv('yeast.data', sep = '\s+' ,header = None)
-ys.columns=['id','mcg','gvh','alm','mit','erl','pox','vac','nuc','class']
-print(ys)
-yt = ys.iloc[:,1:]
-print(yt)
-
-yt['Class']=yt['class']
-yt['Class'] = yt['Class'].map({'MIT': 0, 'NUC': 1, 'CYT': 2, 'ME1': 3, 'EXC': 4, 'ME2': 5, 'ME3': 6, 'VAC': 7, 'POX': 8, 'ERL': 9})
-yt["Class"].unique()
-yt = yt.drop(['class'],axis=1)
-yt.info()
-dataset = yt
-
 def cross_validation(dataset, n_folds):
     dataset_split = list()
     dataset_copy = list(dataset)
@@ -42,17 +29,50 @@ def accuracy_metric(actual,predicted):
             correct+=1
     return correct/float(len(actual)) * 100.0
 
+def precision(class_list, actual, predicted):
+    TP = 0
+    FP = 0
+    for i in range(len(actual)):
+        if predicted[i] == class_list:
+            # print(predicted[i] == class_list)
+            if actual[i] == predicted[i]: 
+                TP +=1
+            else:
+                FP +=1
+    if TP+FP != 0:
+        return (TP/(TP+FP)) * 100.0
+    else:
+        return 0
+    
+def recall(class_list,actual, predicted):
+    TP = 0
+    FN = 0
+    for i in range(len(actual)):
+        if actual[i] == class_list:
+            if actual[i] == predicted[i]:
+                TP += 1
+            else:
+                FN += 1
+    if TP+FN != 0:
+        return (TP/(TP+FN)) * 100.0
+    else:
+        return 0
+
 def evaluate_algorithm(dataset, algorithm, n_folds, method, *args):
+    class_list = list()
+    recall_list = list()
+    precision_list = list()
     folds = cross_validation(dataset, n_folds)
     scores = list()
     score_recall = list()
     score_precision = list()
     x = 1
-    y = 0
     for fold in folds:
         print('Fold ke-%d' % x)
+        recall_l = list()
+        precision_l = list()
         train_set = list(folds)
-        train_set.pop(y)
+        train_set.remove(fold)
         train_set = sum(train_set, [])
         test_set = list()
         for row in fold:
@@ -61,15 +81,27 @@ def evaluate_algorithm(dataset, algorithm, n_folds, method, *args):
             row_copy[-1] = None
         predicted = decision_tree(train_set, test_set, method, *args)
         actual = [row[-1] for row in fold]
+
+        for i in actual:
+            if i not in class_list:
+                class_list.append(i)
+        for i in predicted:
+            if i not in class_list:
+                class_list.append(i)
+        
+        # print(class_list)
+        for i in range(len(class_list)):
+            recall_ret = recall(class_list[i], actual, predicted)
+            precision_ret = precision(class_list[i], actual, predicted) 
+            recall_l.append(recall_ret)
+            precision_l.append(precision_ret)
+        # print()
+        recall_list.append(recall_l)
+        precision_list.append(precision_l)
         accuracy = accuracy_metric(actual, predicted)
-        rec = recall(actual, predicted)
-        pre = precision(actual, predicted)
-        score_recall.append(rec)
-        score_precision.append(pre)
         scores.append(accuracy)
         x+=1
-        y+=1
-    return scores, score_recall, score_precision
+    return scores, recall_list, precision_list, class_list
 
 def test_split(index, value, dataset):
     left, right = list(), list()
@@ -212,15 +244,48 @@ def print_tree(node, depth=0):
     else:
         print('%s [%s]' % ((depth*' ', node)))
 
+ys = pd.read_csv('yeast.data', sep = '\s+' ,header = None)
+ys.columns=['id','mcg','gvh','alm','mit','erl','pox','vac','nuc','class']
+#print(ys)
+yt = ys.iloc[:,1:]
+#print(yt)
+
+yt['Class']=yt['class']
+yt['Class'] = yt['Class'].map({'MIT': 0, 'NUC': 1, 'CYT': 2, 'ME1': 3, 'EXC': 4, 'ME2': 5, 'ME3': 6, 'VAC': 7, 'POX': 8, 'ERL': 9})
+yt["Class"].unique()
+yt = yt.drop(['class'],axis=1)
+yt.info()
 yst = yt.to_numpy()
 
 seed(1)
 dataset = []
+dataset = yst.tolist()
 method = int(input('1. Gini\n2. Entropi\n'))
-n_folds = 3 # diganti ganti sesuai k-fold nya
+n_folds = 3 # diganti ganti sesuai berapa fold yang mau digunakan
 max_depth = 3
 min_size = 10
-scores, score_recall, score_precision = evaluate_algorithm(yst, decision_tree, n_folds, max_depth, min_size)
-print('Mean Accuracy : %.2f%%' % (sum(scores)/float(len(scores))))
-print('Mean Recall : %.2f%%' % (sum(score_recall)/float(len(score_recall))))
-print('Mean Precision : %.2f%%' % (sum(score_precision)/float(len(score_precision))))
+scores, recall_list, precision_list, class_list = evaluate_algorithm(dataset, decision_tree, n_folds, max_depth, min_size)
+
+recall_list2 = np.array(recall_list)
+recall_list2 = recall_list2.transpose()
+precision_list2 = np.array(precision_list)
+precision_list2 = precision_list2.transpose()
+
+hasil_recall = list()
+hasil_precision = list()
+print('Scores: %s' % scores)
+
+# print('Mean Recall : %.3f%%' % (sum(score_recall)/float(len(score_recall))))
+# print('Mean Precision : %.3f%%' % (sum(score_precision)/float(len(score_precision))))
+
+for i in range(len(class_list)):
+    temp1= sum(recall_list2[i])/float(len(recall_list2[i]))
+    temp2= sum(precision_list2[i])/float(len(precision_list2[i]))
+    print('Class: %s' % class_list[i])
+    hasil_recall.append(temp1)
+    hasil_precision.append(temp2)
+    print('Mean Recall : %.3f%%' % (temp1))
+    print('Mean Precision : %.3f%%' % (temp2))
+print('\n\nMean Accuracy akhir : %.3f%%' % (sum(scores)/float(len(scores))))
+print('Mean Recall akhir : %.3f%%' % (sum(hasil_recall)/float(len(hasil_recall))))
+print('Mean Precision akhir : %.3f%%' % (sum(hasil_precision)/float(len(hasil_precision))))
